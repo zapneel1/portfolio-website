a82e0dc1f5ac31301c7d058b8cafe210fdd48073
zapneel1 <130632179+zapneel1@users.noreply.github.com>
2024-10-26 16:54:22 +0530

    Update Layout.tsx

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -17,8 +17,8 @@ export default function Layout(props: ILayoutProps) {
   const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
-    description: `I enjoy creating frontend solutions with solid foundations, scalability, and excellent user experiences.`,
-    image: 'https://pariola.dev/meta/meta-pariola.jpg',
+    description: `I like to craft solid and scalable Open-source products with great user experiences.`,
+    image: 'https://swapneellayek.netlify.app/meta/meta-pariola.jpg',
     type: 'website',
     ...customMeta,
   };
@@ -27,21 +27,21 @@ export default function Layout(props: ILayoutProps) {
       <Head>
         <title>
           {meta.title
-            ? `${meta.title} • Pariola | Creative Frontend Engineer`
-            : 'Pariola | Creative Frontend Engineer'}
+            ? `${meta.title} • Swapneel | Software Engineer`
+            : 'Swapneel | Software Engineer'}
         </title>
         <meta name="robots" content="follow, index" />
         <meta
           name="keyword"
-          content="Omopariola Kehinde Philip, pariola.dev, Pariola, Portfolio, Creative Frontend Developer"
+          content="Swapneel Layek, swapneellayek.netlify.app, Swapneel, swapneel, Portfolio, Software Engineer"
         />
-        <meta name="author" content="Pariola - Creative Frontend Developer" />
+        <meta name="author" content="Swapneel - Software Engineer" />
         <meta content={meta.description} name="description" />
         <meta
           property="og:url"
-          content={`https://pariola.dev${router.asPath}`}
+          content={`https://swapneellayek.netlify.app${router.asPath}`}
         />
-        <link rel="canonical" href={`https://pariola.dev${router.asPath}`} />
+        <link rel="canonical" href={`https://swapneellayek.netlify.app${router.asPath}`} />
         {/*  */}
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Pariola" />
@@ -50,8 +50,8 @@ export default function Layout(props: ILayoutProps) {
           property="og:title"
           content={
             meta.title
-              ? `${meta.title} • Pariola | Creative Frontend Engineer`
-              : 'Pariola | Creative Frontend Engineer'
+              ? `${meta.title} • Swapneel | Software Engineer`
+              : 'Swapneel | Software Engineer'
           }
         />
         <meta property="og:image" content={meta.image} />
@@ -62,8 +62,8 @@ export default function Layout(props: ILayoutProps) {
           name="twitter:title"
           content={
             meta.title
-              ? `${meta.title} • Pariola | Creative Frontend Engineer`
-              : 'Pariola | Creative Frontend Engineer'
+              ? `${meta.title} • Swapneel | Software Engineer`
+              : 'Swapneel | Software Engineer'
           }
         />
         <meta name="twitter:description" content={meta.description} />
